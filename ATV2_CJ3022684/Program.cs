90e9cef7e998fb5c24b99fad1099783e12378dc7
CJ3022684 <CJ3022684@LAB03-PC19>
2024-04-03 12:40:23 -0300

    Adicionar arquivos de projeto.

---
+++ ATV2_CJ3022684/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ATV2_CJ3022684
+{
+
+    static void Main(string[] args)
+    {
+        float salario;
+
+        //leitura de salário
+        Console.WriteLine("Digite um salário");
+        salario = float.Parse(Console.ReadLine());
+
+        salario = salario + (25.0f / 100.0f) * salario;
+        Console.WriteLine("O novo salrio é R$ {0} ", salario);
+        Console.ReadKey();
+    }
+}
